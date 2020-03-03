46a67ebfd98f39c19e68248d89449dbe8e8c570f
jake612 <jake612@live.unc.edu>
2020-03-03 15:50:34 -0500

    fix merge commits

--- src/commit_tree.clj
+++ src/commit_tree.clj
@@ -42,29 +42,30 @@
   (try (let [evaluation (take-while func seq)]
          (if (= (count seq) (count evaluation))
            nil
-           (->> evaluation count (nth seq))))
+           (->> evaluation count (nth seq) second)))
        (catch Exception e nil)))
 
 (defn parent-commit-handler
   "function takes care of the case where there is a p-switch"
-  ([message tree-addr dir db] (parent-commit-handler message tree-addr [] dir db))
-  ([message tree-addr parent-commits dir db]
-   (let [exists-eval (which-true #(.exists (io/as-file (file-path % dir db))) parent-commits)
-         type-eval (which-true #(= (get-object-type % dir db) "commit") parent-commits)
-         commits-concat (fn [x] (reduce str "" (map #(str "parent " % "\n") x)))]
-     (cond
-       (not (nil? exists-eval)) (println (format "Error: no commit object exists at address %s." exists-eval))
-       (not (nil? type-eval)) (println (format "Error: an object exists at address %s, but it isn't a commit." type-eval))
-       :else (-> (commits-concat parent-commits)
-                 (commit-object author_committer tree-addr message)
-                 .getBytes
-                 (wt/write-object dir db)
-                 println)))))
+  [message tree-addr parent-commits dir db]
+  (let [commit-pairs (partition-all 2 parent-commits)
+        exists-eval (which-true #(.exists (io/as-file (file-path (second %) dir db))) commit-pairs)
+        type-eval (which-true #(= (get-object-type (second %) dir db) "commit") commit-pairs)
+        commits-concat (fn [x] (reduce str "" (map #(str "parent " % "\n") x)))]
+    (cond
+      (= (count (last commit-pairs)) 1) (println "Error: you must specify a commit object with the -p switch.")
+      (not (nil? exists-eval)) (println (format "Error: no commit object exists at address %s." exists-eval))
+      (not (nil? type-eval)) (println (format "Error: an object exists at address %s, but it isn't a commit." type-eval))
+      :else (-> (commits-concat (take-nth 2 (rest parent-commits)))
+                (commit-object author_committer tree-addr message)
+                .getBytes
+                (wt/write-object dir db)
+                println))))
 
 (defn commit-tree
   "function for handling commit-tree"
   [args dir db]
-  (let [[tree-addr m-switch message p-switch & parent-commits] args]
+  (let [[tree-addr m-switch message & parent-commits] args]
     (cond
       (not (.isDirectory (io/file dir db))) (println "Error: could not find database. (Did you run `idiot init`?)")
       (nil? tree-addr) (println "Error: you must specify a tree address.")
@@ -72,10 +73,7 @@
       (not= (get-object-type tree-addr dir db) "tree") (println "Error: an object exists at that address, but it isn't a tree.")
       (not= m-switch "-m") (println "Error: you must specify a message.")
       (nil? message) (println "Error: you must specify a message with the -m switch.")
-      (= p-switch "-p") (cond
-                          (nil? parent-commits) (println "Error: you must specify a commit object with the -p switch.")
-                          :else (parent-commit-handler message tree-addr parent-commits dir db))
-      :else (parent-commit-handler message tree-addr dir db))))
+      :else (parent-commit-handler message tree-addr parent-commits dir db))))
 
 
 
