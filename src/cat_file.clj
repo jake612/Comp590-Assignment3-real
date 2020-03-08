1923206dee1504d18243cb679f982b40333dc459
jake612 <jake612@live.unc.edu>
2020-03-07 20:59:20 -0500

    fixed tree print

--- src/cat_file.clj
+++ src/cat_file.clj
@@ -4,13 +4,56 @@
             [clojure.string :as str]
             [commit-tree :as ct]))
 
+(defn to-hex-string
+  "Convert the given byte array into a hex string, 2 characters per byte."
+  [bytes]
+  (letfn [(to-hex [byte]
+            (format "%02x" (bit-and 0xff byte)))]
+    (->> bytes (map to-hex) (apply str))))
+
+(defn tree-entry-formatter
+  "given a single entry from within a tree object, returns formatted string"
+  [header-bytes address-bytes]
+  (let [header-string-list (-> header-bytes fio/bytes->str (str/split #" "))
+        mode (first header-string-list)
+        name (second header-string-list)
+        address (to-hex-string address-bytes)
+        mode (if (= "40000" mode)
+               (str "0" mode)
+               mode)
+        type (if (= mode "04000")
+               "tree"
+               "blob")]
+    (str mode " " type " " address "\t" name "\n")))
+
+(defn format-tree-output
+  [content-bytes]
+  (let [split-bytes (->> content-bytes
+                         (partition-by (partial = 0x00))
+                         (take-nth 2))
+        len-1 (-> split-bytes count (- 1))]
+    (loop [n 1
+           header-bytes (first split-bytes)
+           string ""]
+      (if (> n len-1)
+        string
+        (let [all-info (->> (nth split-bytes n) (split-at 20))
+              address-bytes (first all-info)
+              next-header-bytes (second all-info)]
+          (recur (inc n) next-header-bytes (str string (tree-entry-formatter header-bytes address-bytes))))))))
+
+
 (defn cat-file
   "function for handling cat-file command"
   [args dir db]
   (let [switch (first args)
         address (second args)
         get-path #(str dir db "/objects/" (subs % 0 2) "/" (subs % 2))
-        splitter #(str/split % #"\000")]
+        get-content-bytes #(->> %
+                               get-path
+                               fio/unzip
+                               (fio/split-at-byte (byte 0x00))
+                               second)]
     (cond
       (not (.isDirectory (io/file dir db))) (println "Error: could not find database. (Did you run `idiot init`?)")
       (and (not= switch "-p") (not= switch "-t")) (println "Error: the -p or -t switch is required")
@@ -19,18 +62,13 @@
       :else (case switch
               "-p" (if (= (ct/get-object-type address dir db) "tree")
                      (->> address
-                          get-path
-                          fio/unzip
-                          (fio/split-at-byte (byte 0x00))
-                          count
-                          println)
+                          get-content-bytes
+                          format-tree-output
+                          print)
                      (->> address
-                          get-path
-                          fio/unzip
+                          get-content-bytes
                           (map char)
                           (apply str)
-                          splitter
-                          second
                           print))
               "-t" (-> address
                        (ct/get-object-type dir db)
